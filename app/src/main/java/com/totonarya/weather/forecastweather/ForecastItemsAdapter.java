4ca7830c76188974def9ab626ff7dfbbd33c5be9
unknown <mahmoudalipour.d@gmail.com>
2020-05-14 15:16:18 +0430

    n

--- app/src/main/java/com/totonarya/weather/forecastweather/ForecastItemsAdapter.java
+++ app/src/main/java/com/totonarya/weather/forecastweather/ForecastItemsAdapter.java
@@ -1,12 +1,31 @@
 package com.totonarya.weather.forecastweather;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class ForecastItemsAdapter{
+import com.totonarya.weather.R;
+
+public class ForecastItemsAdapter extends RecyclerView.Adapter<ForecastItemsAdapter.MyViewHolder>{
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class MyViewHolder extends RecyclerView.ViewHolder{
         public TextView one;
         public TextView two;
@@ -18,7 +37,13 @@ public class ForecastItemsAdapter{
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            one = itemView.findViewById(R.id)
+            one = itemView.findViewById(R.id.txt_forecastItems_One);
+            two = itemView.findViewById(R.id.txt_forecastItems_Two);
+            three = itemView.findViewById(R.id.txt_forecastItems_Three);
+            four = itemView.findViewById(R.id.txt_forecastItems_Four);
+            five = itemView.findViewById(R.id.txt_forecastItems_Five);
+            six = itemView.findViewById(R.id.txt_forecastItems_Six);
+            seven = itemView.findViewById(R.id.txt_forecastItems_Seven);
         }
     }
 
