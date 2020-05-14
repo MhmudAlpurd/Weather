a35daccd209dcc311450cd587c7ac439219e9ca3
unknown <mahmoudalipour.d@gmail.com>
2020-05-14 13:28:36 +0430

    future temperature is drawn in forecastActivity

--- app/src/main/java/com/totonarya/weather/forecastweather/ForecastActivity.java
+++ app/src/main/java/com/totonarya/weather/forecastweather/ForecastActivity.java
@@ -5,23 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 import com.totonarya.weather.R;
 import com.totonarya.weather.data.WeatherRepository;
 import com.totonarya.weather.data.pojo.forecast.ForecastWeather;
+import com.totonarya.weather.utils.Func;
+
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.LegendModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ForecastActivity extends AppCompatActivity implements ForecastContract.View {
     ForecastContract.Presenter presenter;
+    List<Integer> tempsList = new ArrayList<>();
     String city = "Tehran";
     String state = "Iran";
+    Func func = new Func();
+    BarChart mBarChart;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
-        presenter = new ForecastPresenter(new WeatherRepository(),city,state);
+        presenter = new ForecastPresenter(new WeatherRepository(), city, state);
+        mBarChart = findViewById(R.id.barChart);
 
     }
 
@@ -32,14 +48,28 @@ public class ForecastActivity extends AppCompatActivity implements ForecastContr
 
     @Override
     public void showForecastWeather(ForecastWeather forecastWeather) {
-        Log.d("LLL", forecastWeather.getCity().getName());
 
+        int d = forecastWeather.getList().size();
+        Double f = forecastWeather.getList().get(0).getMain().getTemp();
+
+        for (int i = 0; i < d; i++) {
+            Double tempList_Kelvin = forecastWeather.getList().get(i).getMain().getTemp();
+            String date = forecastWeather.getList().get(i).getDt().toString();
+            int tempList_Converted = func.kelvinToCenti(tempList_Kelvin);
+            tempsList.add(tempList_Converted);
+            int color = func.color_Selector(tempList_Converted);
+            mBarChart.addBar(new BarModel((float) tempList_Converted, color));
+           //TODO: SET DATES INSTEAD OF LEGENDS OF BARCHART.
+        }
+
+        mBarChart.startAnimation();
 
     }
 
     @Override
     public void showError(String error) {
-      Log.d("LLL",error);
+        Log.d("LLL", error);
+        Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
     }
 
     @Override
--- app/src/main/java/com/totonarya/weather/utils/Func.java
+++ app/src/main/java/com/totonarya/weather/utils/Func.java
@@ -39,5 +39,74 @@ public class Func {
 
     }
 
+    public int color_Selector(double temperature) {
+        int color;
+        if (temperature < -40) {
+            color = 0xFF818281;
+        } else if (-40 <= temperature && temperature < -37) {
+            color = 0xFFcccccd;
+        } else if (-37 <= temperature && temperature < -34.5) {
+            color = 0xFFffffff;
+        } else if (-34.5 <= temperature && temperature < -31.5) {
+            color = 0xFFfde6ff;
+        } else if (-31.5 <= temperature && temperature < -29) {
+            color = 0xFFfac8fe;
+        } else if (-29 <= temperature && temperature < -26) {
+            color = 0xFFf664fd;
+        } else if (-26 <= temperature && temperature < -23) {
+            color = 0xFFe104e2;
+        } else if (-23 <= temperature && temperature < -20) {
+            color = 0xFFb164c0;
+        } else if (-20 <= temperature && temperature < -18) {
+            color = 0xFF8c01af;
+        } else if (-18 <= temperature && temperature < -15) {
+            color = 0xFF0c0096;
+        } else if (-15 <= temperature && temperature < -12) {
+            color = 0xFF1400c8;
+        } else if (-12 <= temperature && temperature < -9.5) {
+            color = 0xFF233cfd;
+        } else if (-9.5 <= temperature && temperature < -6.5) {
+            color = 0xFF338cf0;
+        } else if (-6.5 <= temperature && temperature < -4) {
+            color = 0xFF41b5fd;
+        } else if (-4 <= temperature && temperature < -1) {
+            color = 0xFF96e7fe;
+        } else if (-1 <= temperature && temperature < 1.5) {
+            color = 0xFF217802;
+        } else if (1.5 <= temperature && temperature < 4.5) {
+            color = 0xFF32a131;
+        } else if (4.5 <= temperature && temperature < 7) {
+            color = 0xFF3ec805;
+        } else if (7 <= temperature && temperature < 10) {
+            color = 0xFF64e764;
+        } else if (10 <= temperature && temperature < 13) {
+            color = 0xFF8cff8c;
+        } else if (13 <= temperature && temperature < 15.5) {
+            color = 0xFFb4ffb5;
+        } else if (15.5 <= temperature && temperature < 18) {
+            color = 0xFFfafba0;
+        } else if (18 <= temperature && temperature < 21) {
+            color = 0xFFf9f673;
+        } else if (21 <= temperature && temperature < 24) {
+            color = 0xFFf5dd5a;
+        } else if (24 <= temperature && temperature < 27) {
+            color = 0xFFf7b429;
+        } else if (27 <= temperature && temperature < 29.5) {
+            color = 0xFFf08c12;
+        } else if (29.5 <= temperature && temperature < 32) {
+            color = 0xFFdc5004;
+        } else if (32 <= temperature && temperature < 38) {
+            color = 0xFFf0273c;
+        } else if (38 <= temperature && temperature < 40.5) {
+            color = 0xFFb42703;
+        } else if (40.5 <= temperature && temperature < 43.5) {
+            color = 0xFFfac8dc;
+        } else if ( temperature <= 43.5) {
+            color = 0xFF8c0101;
+        }else {
+            color = 0xFF8c0101;
+        }
+            return color;
+        }
 
-}
+    }
