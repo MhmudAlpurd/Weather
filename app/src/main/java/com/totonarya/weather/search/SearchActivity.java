86c5498162beca9ec285350db9cdb3975eea14ed
unknown <mahmoudalipour.d@gmail.com>
2020-05-28 14:48:25 +0430

    search is created

---
+++ app/src/main/java/com/totonarya/weather/search/SearchActivity.java
@@ -0,0 +1,20 @@
+package com.totonarya.weather.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.totonarya.weather.R;
+
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+
+
+
+    }
+}
